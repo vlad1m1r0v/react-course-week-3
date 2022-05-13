34e82016dbe37a385ba32ad0620b3348d6c053b6
= <mypochtav@gmail.com>
2022-05-13 14:31:56 +0300

    add form in contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,51 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Form,
+  Col,
+  Row,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 export default function Contact() {
+  const [state, setState] = useState({
+    firstname: "",
+    lastname: "",
+    telnum: "",
+    email: "",
+    agree: false,
+    contactType: "Tel.",
+    message: "",
+  });
+
+  function handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    setState({ ...state, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    console.log("Current State is: " + JSON.stringify(state));
+    alert("Current State is: " + JSON.stringify(state));
+    event.preventDefault();
+  }
   return (
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Contact Us</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row row-content">
         <div className="col-12">
           <h3>Location Information</h3>
@@ -45,6 +90,110 @@ export default function Contact() {
               <i className="fa fa-envelope-o"></i> Email
             </a>
           </div>
+        </div>
+      </div>
+      {/*form*/}
+      <div className="row row-content">
+        <div className="col-12">
+          <h3>Send us your Feedback</h3>
+        </div>
+        <div className="col-12 col-md-9">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Row} className="mb-3" controlId="formFirstName">
+              <Form.Label column sm={2}>
+                First name
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  type="text"
+                  placeholder="First name"
+                  name="firstname"
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formLastName">
+              <Form.Label column sm={2}>
+                Last name
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  type="text"
+                  placeholder="Last name"
+                  name="lastname"
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formLastName">
+              <Form.Label column sm={2}>
+                Contact Tel.
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  type="tel"
+                  placeholder="Tel. Number"
+                  name="telnum"
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formEmail">
+              <Form.Label column sm={2}>
+                Email
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formCheckBox">
+              <Col md={{ size: 6, offset: 2 }}>
+                <Form.Check
+                  type="checkbox"
+                  id={`default-checkbox`}
+                  label={<strong>May we contact you?</strong>}
+                  name="agree"
+                  onChange={handleInputChange}
+                />
+              </Col>
+              <Col md={{ size: 2, offset: 1 }}>
+                <Form.Select
+                  aria-label="Default select example"
+                  name="contactType"
+                  onChange={handleInputChange}
+                >
+                  <option>Tel.</option>
+                  <option>Email</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formMessage">
+              <Form.Label column sm={2}>
+                Your Feedback
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  as="textarea"
+                  rows={10}
+                  name="message"
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row}>
+              <Col md={{ size: 10, offset: 2 }}>
+                <Button type="submit" color="primary">
+                  Send Feedback
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
         </div>
       </div>
     </div>
